2bc35ad4c107d4d22944ae70be5f2e788e52d854
Dipinder Sharma <dipinder2@gmail.com>
2021-07-14 20:36:48 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import {Router} from '@reach/router'
+import Search from './components/Search'
+import DisplayItem from './components/DisplayItem'
+import {useState} from 'react'
 function App() {
+
+
   return (
     <div className="App">
-
+        <Search/>
+        <Router>
+          <DisplayItem path="/:term/:id"/>
+        </Router>
     </div>
   );
 }
